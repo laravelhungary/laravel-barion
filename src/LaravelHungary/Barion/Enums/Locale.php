dd3317a6704be23c4ea92ca91b2469cf65de570d
Zsolt Fekete <nxu@nxu.hu>
2021-11-09 14:02:26 +0100

    Add all supported locales

--- src/LaravelHungary/Barion/Enums/Locale.php
+++ src/LaravelHungary/Barion/Enums/Locale.php
@@ -4,13 +4,12 @@ namespace LaravelHungary\Barion\Enums;
 
 class Locale
 {
-    /**
-     * Hungarian locale.
-     */
-    const HU = 'hu-HU';
-
-    /**
-     * US English locale.
-     */
+    const CZ = 'cs-CZ';
+    const DE = 'de-DE';
     const EN = 'en-US';
+    const ES = 'es-ES';
+    const FR = 'fr-FR';
+    const HU = 'hu-HU';
+    const SK = 'sk-SK';
+    const SI = 'sl-SI';
 }
